236f531062f46333a6f6cf5faa02dd4428a8d430
Mateusz Winiarski <mateusz.winiarski.94@gmail.com>
2017-09-24 22:08:30 +0200

    Replace Window by MetroWindow and implement HamburgerMenu

--- PracaMagisterska.WPF/MainWindow.xaml.cs
+++ PracaMagisterska.WPF/MainWindow.xaml.cs
@@ -1,10 +1,19 @@
-﻿using System.Windows;
+﻿using System.Threading;
+using System.Windows;
+using System.Windows.Navigation;
+using MahApps.Metro.Controls;
+using PracaMagisterska.WPF.Utils;
 
 namespace PracaMagisterska.WPF {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window {
-		public MainWindow() { InitializeComponent(); }
-	}
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow {
+        public MainWindow() { InitializeComponent(); }
+
+        private void HamburgerMenuControl_OnItemClick(object sender, ItemClickEventArgs e) {
+            HamburgerMenuControl.Content = e.ClickedItem;
+            HamburgerMenuControl.IsPaneOpen = false;
+        }
+    }
 }
