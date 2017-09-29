d19f0618d37f9cd9adc0be710ab61b95cfc91201
Mateusz Winiarski <mateusz.winiarski.94@gmail.com>
2017-09-29 14:19:15 +0200

    Add credit page

---
+++ PracaMagisterska/View/Credits.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Controls;
+
+namespace PracaMagisterska.WPF.View {
+    /// <summary>
+    /// Interaction logic for Credits.xaml
+    /// </summary>
+    public partial class Credits : UserControl {
+        public Credits() => InitializeComponent();
+    }
+}
