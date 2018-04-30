58ebed5f486605308fef1d17e41adfdf1a4504e8
Mateusz Winiarski <mateusz.winiarski.94@gmail.com>
2018-04-30 17:07:06 +0200

    Add simple MagicalNumber refactorer

--- PracaMagisterska/CodeRules.cs
+++ PracaMagisterska/CodeRules.cs
@@ -54,7 +54,7 @@ namespace PracaMagisterska.WPF {
             => root.DescendantNodes()
                    .Where(node => Settings.SyntaxKindsForMagicalNumberSearch.Any(kind => kind == node.Kind()))
                    .Where(node => node.DescendantTokens().Any(s => s.Kind() == SyntaxKind.NumericLiteralToken))
-                   .Select(node => DiagnosticHelper.Create(node, "Numerical literal found in expression", null));
+                   .Select(node => DiagnosticHelper.Create(node, "Numerical literal found in expression", new MagicalNumberRemoval()));
 
         /// <summary>
         /// This method finds all method which is not using all of its parameters
---
+++ PracaMagisterska/Utils/Rewriters/MagicalNumberRemoval.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.Text;
+using static PracaMagisterska.WPF.Utils.CompilationHelper;
+
+namespace PracaMagisterska.WPF.Utils.Rewriters {
+    public class MagicalNumberRemoval : CSharpSyntaxRewriter, IRefactor {
+        /// <summary>
+        /// Visit Expression Statement and removes from it magical numbers
+        /// </summary>
+        /// <param name="node">Visited statement</param>
+        /// <returns>New block of code with replaced magical number in expresion</returns>
+        public override SyntaxNode VisitExpressionStatement(ExpressionStatementSyntax node) {
+            List<SyntaxNode> numericalNodes = node.DescendantNodes()
+                                                  .Where(n => n.Kind() == SyntaxKind.NumericLiteralExpression)
+                                                  .ToList();
+
+            if ( numericalNodes.Count > 0 ) {
+                BlockSyntax statementList = SyntaxFactory.Block()
+                                                         .WithOpenBraceToken(SyntaxFactory.MissingToken(SyntaxKind.OpenBraceToken))
+                                                         .WithCloseBraceToken(SyntaxFactory.MissingToken(SyntaxKind.CloseBraceToken));
+
+                string variableName = "magicalNumber";
+                for ( int i = 0; i < numericalNodes.Count; ++i ) {
+                    LocalDeclarationStatementSyntax expresion = (LocalDeclarationStatementSyntax)SyntaxFactory.ParseStatement("var " + variableName + (i + 1) + " = " + numericalNodes[i].ToFullString() + ";")
+                                                                                                 .WithTriviaFrom(node)
+                                                                                                 .WithTrailingTrivia(SyntaxFactory.ParseTrailingTrivia("\n"));
+
+                    statementList = statementList.AddStatements(expresion);
+
+                    node = node.ReplaceNode(numericalNodes[i], SyntaxFactory.IdentifierName(expresion.Declaration.Variables[0].Identifier));
+                }
+
+                return statementList.AddStatements(node.WithTrailingTrivia(SyntaxFactory.ParseTrailingTrivia("\n")));
+            } else {
+                return node;
+            }
+        }
+
+        /// <inheritdoc />
+        public SyntaxTree Refactor(SyntaxNode nodeToRefactor)
+            => nodeToRefactor.SyntaxTree.GetRoot()
+                             .ReplaceNode(nodeToRefactor.Parent,
+                                          VisitExpressionStatement(SyntaxFactory.ExpressionStatement((ExpressionSyntax)nodeToRefactor)))
+                             .SyntaxTree;
+    }
+}
