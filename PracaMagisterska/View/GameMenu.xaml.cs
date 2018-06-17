07992f645b6c4451db4ca5486c0e9d0e0241b030
Mateusz Winiarski <mateusz.winiarski.94@gmail.com>
2018-06-17 18:08:14 +0200

    Update GameMenu.xaml.cs

--- PracaMagisterska/View/GameMenu.xaml.cs
+++ PracaMagisterska/View/GameMenu.xaml.cs
@@ -16,13 +16,13 @@ namespace PracaMagisterska.WPF.View {
 
             DescriptionTextBlock.Text = @"Cześć!
             
-Po lewej stronie znajdują się poszczególne lekcje. Możesz wybrać dowolną, ale zdecydowanie zalecam wybieranie ich i rozwiązywanie pokolei. Zapewne dość często będzie sie zdażać tak, że jedna z lekcji będzie wymagała wiedzy z poprzednich. Tak to już jest z programowaniem, że bez podstaw nie da sie zrobić rzeczy bardziej zaawansowanych.
-Jeżeli dla lekcji istnieją jakieś testy możesz je uruchomić odpowiednim przyciskiem. Jeśli wszystkie testy zakończą się pozytywnie Twoje rozwiązanie zostanie ocenione zarówno pod kontem ilości linii kodu jak i czasu wykonania.
-Rodzaje testów z jakimi się spotkasz:
-- Testy statyczne - sprawdzają czy użyłeś właściwych konstrukcji (np. czy w lekcji z warunkami użyłeś konstrukci z if-em).
-- Proste testy - kilka prostych testów pokazujących jak Twoje rozwiązanie ma się zachowywać w konkretnych sytuacjach.
-- Testy rzeczywiste - sporo testów, które zawieraja w sobie np. testowanie wartosci granicznych. Mają one dać pełen obraz tego, czy Twoje rozwiązanie jest poprawne.
-- Testy losowe - mają poprostu za każdym razem losowo wygenerowane parametry (oczywiście takie, które nie znajdują się w testach prostych czy rzeczywistych). Mają na celu głównie zabezpieczenie się przed programami napisanymi tak, by przechodzić testy, a nie tak, żeby być kompleksowymi i poprawnymi rozwiązaniami.
+Po lewej stronie znajdują się poszczególne lekcje. Możesz wybrać dowolną, ale zdecydowanie zalecam wybieranie ich i rozwiązywanie po kolei. Zapewne dość często będzie sie zdarzać, że jedna z lekcji będzie wymagała wiedzy z poprzednich. Tak to już jest z programowaniem, że bez podstaw nie da się zrobić rzeczy bardziej zaawansowanych.
+Jeżeli dla lekcji istnieją jakieś testy, możesz je uruchomić odpowiednim przyciskiem. Jeśli wszystkie testy zakończą się pozytywnie, Twoje rozwiązanie zostanie ocenione zarówno pod kątem liczby linii kodu, jak i czasu wykonania.
+Rodzaje testów, z jakimi się spotkasz:
+- Test statyczny - sprawdzają, czy użyłeś właściwych konstrukcji (np. czy w lekcji z warunkami użyłeś konstrukcji z if-em).
+- Proste testy - kilka prostych testów pokazujących, jak Twoje rozwiązanie ma się zachowywać w konkretnych sytuacjach.
+- Testy rzeczywiste - sporo testów, które zawierają w sobie np. testowanie wartości granicznych. Mają one dać pełen obraz tego, czy Twoje rozwiązanie jest poprawne.
+- Testy losowe - mają po prostu za każdym razem losowo wygenerowane parametry (oczywiście takie, które nie znajdują się w testach prostych czy rzeczywistych). Mają na celu głównie zabezpieczenie się przed programami napisanymi tak, by przechodzić testy, a nie tak, żeby być kompleksowymi i poprawnymi rozwiązaniami.
 Pamiętaj, że zawsze masz do dyspozycji przycisk ""Uruchom"", który będzie uruchamiał metodę: public static void Main(string args). Możesz w niej sprawdzić działanie swojej funkcji przed puszczeniem właściwych testów (o ile takie istnieją dla danej lekcji).";
         }
 
