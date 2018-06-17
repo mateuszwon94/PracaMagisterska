d9e3a4192201d74651c08ccc58f185296284f34a
Mateusz Winiarski <mateusz.winiarski.94@gmail.com>
2018-06-17 18:11:03 +0200

    Update Lesson0.cs

--- PracaMagisterska/Testers/Lesson0.cs
+++ PracaMagisterska/Testers/Lesson0.cs
@@ -20,9 +20,9 @@
         
         /// <inheritdoc cref="Lesson.Info"/>
         public override string Info { get; } =
-@"Oto twój pierwszy program. Przejdźmy linijka po linijce i wytłumacze Ci co się w nim dzieje.
-W pierwszej linijce przy pomocy słowa kluczowego using importowana jest podstawowa biblioteka. W niej zanjdują się wszystkie najprostsze i najpotrzebniejsze typy typy.
-Ponieważ C# jest czysto obiektowym językiem, to w linijce trzeciej zdefiniowana jest klasa (czy to dokładnie jest dowiesz się później). Ma ona w sobie metode Main. Tak nazywa się metodę, którą system operacyjny uruchamia jako pierwszą. W jej ciele z obiektu Console wywoływana jest metoda WriteLine, która wypisuje na ekran tekst z cudzysłowia.
+@"Oto Twój pierwszy program. Przejdźmy linijka po linijce i wytłumaczę Ci, co się w nim dzieje.
+W pierwszej linijce przy pomocy słowa kluczowego using importowana jest podstawowa biblioteka. W niej znajdują się wszystkie najprostsze i najpotrzebniejsze typy.
+Ponieważ C# jest czysto obiektowym językiem, to w linijce trzeciej zdefiniowana jest klasa (czym to dokładnie jest, dowiesz się później). Ma ona w sobie metodę Main. Tak nazywa się metodę, którą system operacyjny uruchamia jako pierwszą. W jej ciele z obiektu Console wywoływana jest metoda WriteLine, która wypisuje na ekran tekst z cudzysłowu.
 Spróbuj skompilować i uruchomić ten program. Co zobaczyłeś w konsoli? Teraz spróbuj zmienić tekst i uruchomić program jeszcze raz.";
     }
 }
